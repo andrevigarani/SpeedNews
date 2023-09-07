367d6c3702acaa0a0df72aaa4e120ba979c20ea5
André Felipe Vigarani <avlipe44@gmail.com>
2023-09-07 18:14:46 -0300

    First Commit

---
+++ src/main/java/com/speednews/SpeedNews/SpeedNewsApplication.java
@@ -0,0 +1,13 @@
+package com.speednews.SpeedNews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpeedNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpeedNewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/speednews/SpeedNews/SpeedNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.speednews.SpeedNews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeedNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
